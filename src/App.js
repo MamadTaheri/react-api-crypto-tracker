5270e5248ef87b891af903fc1756f9e2ac2b4d84
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-06 16:34:53 +0430

    add state and effect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+         .then(response => {
+           setCoins(response.data);
+         })
+         .catch(error => {
+           alert("Api error")
+         });
+    
+  }, []);
+
+
   return (
     <div className="App">
       <h1>APIIIIII</h1>
